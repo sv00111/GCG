f0dd944d90eb20fd2a60d3583fe46d21409afab3
Shrey Valia <svalia@ucsc.edu>
2016-02-23 12:08:28 -0800

    updated gitignore and python program now can pick a random word from a specified list

--- GCGProgram.py
+++ GCGProgram.py
@@ -1,15 +1,41 @@
+import sys
+import random
+
+
 def main():
-	print "Hello World"
-	helpingVerbs = [line.rstrip('\n') for line in open('helpingVerbs.txt')]
-	intransitiveVerbs = [line.rstrip('\n') for line in open('intransitiveVerbs.txt')]
-	transitiveVerbs = [line.rstrip('\n') for line in open('transitiveVerbs.txt')]
-	print "helping: "
-	print("\n".join(helpingVerbs))
-	print "\n\nintranstive: "
-	print("\n".join(intransitiveVerbs))
-	print "\n\ntransitiveVerbs: "
-	print("\n".join(transitiveVerbs))
-	
+    print "Hello World"
+    helpingVerbs = [line.rstrip('\n') for line in open('helpingVerbs.txt')]
+    intransitiveVerbs = [line.rstrip('\n') for line in open('intransitiveVerbs.txt')]
+    transitiveVerbs = [line.rstrip('\n') for line in open('transitiveVerbs.txt')]
+    adjectives = [line.rstrip('\n') for line in open('adjectives.txt')]
+    adverbs = [line.rstrip('\n') for line in open('adverbs.txt')]
+    determiners = [line.rstrip('\n') for line in open('determiners.txt')]
+    linkingVerbs = [line.rstrip('\n') for line in open('linkingVerbs.txt')]
+    nouns = [line.rstrip('\n') for line in open('nouns.txt')]
+    pluralDeterminers = [line.rstrip('\n') for line in open('pluralDeterminers.txt')]
+    pluralNouns = [line.rstrip('\n') for line in open('pluralNouns.txt')]
+    prepositions = [line.rstrip('\n') for line in open('prepositions.txt')]
+
+
+    picking = True
+    listsDictionary = {"helpingVerbs":helpingVerbs, "intransitiveVerbs":intransitiveVerbs, "transitiveVerbs":transitiveVerbs,
+                       "adjectives":adjectives, "adverbs":adverbs, "determiners":determiners, "linkingVerbs":linkingVerbs,
+                       "nouns":nouns, "pluralDeterminers":pluralDeterminers, "pluralNouns":pluralNouns, "prepositions":prepositions,
+                       }
+
+    while(picking):
+        name = raw_input("what do you want to pick out of? (write quit to quit)\n-> ")
+        if (name == 'quit'):
+            picking = False
+            continue
+        print "name is: " + name
+        try:
+            print("word is: " + random.choice(listsDictionary[name]))
+        except NameError:
+            print "list errored"
+
+
+
 
 
 if __name__ == "__main__":
